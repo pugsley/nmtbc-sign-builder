01fc19af9c31e82906595b785e465188eea372af
Tama Pugsley <tama@sharesies.co.nz>
2025-11-24 08:20:27 +1300

    fix local dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/nmtbc-sign-builder/',
-})
+  base: command === 'build' ? '/nmtbc-sign-builder/' : '/',
+}))
